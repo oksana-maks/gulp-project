8cf66a5223ddbb9805fddb7fadbd53b65e927712
zakharkiv_oksana <oksanamaks92@gmail.com>
2021-10-21 20:38:54 +0300

    create base gupl project

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+// Class Person
+
+function Person(name, sname, age){
+    this.firstName = name;
+    this.secondName = sname;
+    this.age = age;
+}
+const newPerson = new Person('Ivan', 'Ivanov', 25);
+console.log('newPerson', newPerson);
